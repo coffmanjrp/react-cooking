8a5c4cd460047c25704ccea7c26e5a413008902d
coffmanjrp <coffmanjrp@gmail.com>
2022-01-15 14:55:53 -0800

    Making a Create Recipe Form

--- src/pages/create/Create.tsx
+++ src/pages/create/Create.tsx
@@ -1,8 +1,53 @@
-import { FC } from 'react';
+import { FC, FormEvent, useState } from 'react';
 import './Create.css';
 
 const Create: FC = () => {
-  return <div>Create</div>;
+  const [title, setTitle] = useState('');
+  const [method, setMethod] = useState('');
+  const [cookingTime, setCookingTime] = useState('');
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    console.log(title, method, cookingTime);
+  };
+
+  return (
+    <div className="create">
+      <h2 className="page-title">Add a New Recipe</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Recipe title:</span>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          <span>Recipe method:</span>
+          <textarea
+            value={method}
+            onChange={(e) => setMethod(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          <span>Cooking time (minutes):</span>
+          <input
+            type="number"
+            value={cookingTime}
+            onChange={(e) => setCookingTime(e.target.value)}
+            required
+          />
+        </label>
+        <button type="submit" className="btn">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Create;
