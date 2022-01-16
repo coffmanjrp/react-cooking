fbe0ad4b2121c64fd79b82d33cae3e4bf03a965a
coffmanjrp <coffmanjrp@gmail.com>
2022-01-16 15:28:21 -0800

    Show Message When No Search Result

--- src/components/RecipeList.tsx
+++ src/components/RecipeList.tsx
@@ -8,6 +8,10 @@ type Props = {
 };
 
 const RecipeList: FC<Props> = ({ recipes }) => {
+  if (recipes.length === 0) {
+    return <div className="error">No recipes to load...</div>;
+  }
+
   return (
     <div className="recipe-list">
       {recipes.map((recipe) => (
