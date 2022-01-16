a2a91442ee642c35193097cdbfff68fe2d9a7d87
coffmanjrp <coffmanjrp@gmail.com>
2022-01-16 15:13:05 -0800

    Search Results Page

--- src/pages/search/Search.tsx
+++ src/pages/search/Search.tsx
@@ -1,8 +1,23 @@
 import { FC } from 'react';
+import { useLocation } from 'react-router-dom';
+import { RecipeList } from 'components';
+import useFetch from 'hooks/useFetch';
 import './Search.css';
 
 const Search: FC = () => {
-  return <div>Search</div>;
+  const queryString = useLocation();
+  const queryParams = new URLSearchParams(queryString.search);
+  const query = queryParams.get('q');
+  const { data, isPending, error } = useFetch(`/recipes?q=${query}`);
+
+  return (
+    <div>
+      <h2 className="page-title">Recipes including "{query}"</h2>
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {data && <RecipeList recipes={data} />}
+    </div>
+  );
 };
 
 export default Search;
